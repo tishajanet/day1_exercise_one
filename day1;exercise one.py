58ec154dcd55216805d730365853473080d6db19
tishajanet <tishajanet@students.uonbi.ac.ke>
2025-11-27 23:07:08 +0300

    2nd commit

--- day1;exercise one.py
+++ day1;exercise one.py
@@ -1,7 +1,7 @@
 import sys
 def main():
     name = input()
-    print(f"Hello, <{name}>!")
+    print(f"Hello, <{name}>!") #tested with name Tisha
     return 0
 if __name__ == '__main__':
     sys.exit(main())
